8102aa990da4fd0737e6f485806b3a083dd11f6c
Gases <aariasmartijena@gmail.com>
2022-06-07 17:11:36 -0400

    deploy to gh-pages

--- src/routes/nosotros/Etica.jsx
+++ src/routes/nosotros/Etica.jsx
@@ -52,19 +52,3 @@ export default function Etica() {
     </div>
   );
 }
-
-{
-  /* <h2>Documentos importantes</h2>
-      <a
-        href="https://www.dropbox.com/s/y6i0etgquhhywl3/C%C3%B3digo%20de%20%C3%89tica%20y%20Conducta%20Institucional%20Res%20OC-19-2018.100.pdf?dl=0"
-        target="_blank"
-      >
-        Código de ética
-      </a>
-      <a
-        href="https://www.dropbox.com/s/2jdz8rbsjxz3n4o/Pol%C3%ADtica%20Conflictos%20de%20Intereses.pdf?dl=0"
-        target="_blank"
-      >
-        Política de conflictos de intereses
-      </a> */
-}
